59adb290570d0b453f3c052217d7582aaaaad8d1
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-07 13:24:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-   text.scan(/\b(\w[un])|([ing]{3})/)
+  arr = []
+  arr = text.scan(/\b(\w[un])|([ing]{3})/)
    binding.pry
 end
 
